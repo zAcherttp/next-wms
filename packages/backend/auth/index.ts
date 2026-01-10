10f0c0beeac74d485aebd3896259931b0518a161
zacher <Saladegg24@gmail.com>
2026-01-10 18:41:59 +0700

    chore: update dependencies in pnpm-workspace.yaml
    - Bump @types/react from 19.2.2 to 19.2.8
    - Bump @types/react-dom from 19.2.2 to 19.2.3
    - Add @types/node at version 25.0.5
    - Add typescript at version 5.9.3
    - Bump convex from 1.31.2 to 1.31.3
    - Bump next from 16.0.0 to 16.1.1
    - Bump react from 19.2.0 to 19.2.3
    - Bump react-dom from 19.2.0 to 19.2.3
    - Bump zod from 4.1.12 to 4.3.5

--- packages/backend/auth/index.ts
+++ packages/backend/auth/index.ts
@@ -1,5 +1,5 @@
+import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { betterAuth } from "better-auth/minimal";
 import { emailOTP, openAPI, organization } from "better-auth/plugins";
 import { ConvexHttpClient } from "convex/browser";
 import { api } from "../convex/_generated/api";
